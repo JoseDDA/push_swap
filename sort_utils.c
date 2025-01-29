67aff65551447b8b4108b36331079aab519b0368
root <root@LAPTOP-KE2O475O.>
2025-01-29 22:59:31 +0100

    updated find_max y find_index

--- sort_utils.c
+++ sort_utils.c
@@ -6,29 +6,27 @@
 /*   By: jdorazio <jdorazio@student.42.madrid.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 16:41:02 by jdorazio          #+#    #+#             */
-/*   Updated: 2025/01/28 17:44:05 by jdorazio         ###   ########.fr       */
+/*   Updated: 2025/01/29 15:25:53 by jdorazio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "push_swap.h"
 
-size_t	find_index(t_stack *stacks, int value)
+size_t	find_index(int *stack, size_t size, int value)
 {
 	size_t	i;
-	int		*stack;
 
 	i = 0;
-	stack = stacks->stack_a;
-	while (i < stacks->size_a)
+	while (i < size)
 	{
 		if (stack[i] == value)
 			return (i);
 		i++;
 	}
-	//printf("Value %d not found in stack_a\n", min_value);
-
 	return (i);
 }
+
+
 
 int	find_min(t_stack *stacks)
 {
@@ -47,17 +45,17 @@ int	find_min(t_stack *stacks)
 	return (min_value);
 }
 
-int	find_max(t_stack *stacks)
+int	find_max(int *stack, size_t size)
 {
 	size_t	i;
 	int	max_value;
 
 	i = 0;
 	max_value = INT_MIN;
-	while (i < stacks->size_a)
+	while (i < size)
 	{
-		if (stacks->stack_a[i] > max_value)
-			max_value = stacks->stack_a[i];
+		if (stack[i] > max_value)
+			max_value = stack[i];
 		i++;
 	}
 	return (max_value);
