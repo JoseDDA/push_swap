381b4bfa263e5de3388e7c0274e3b57d7a1b7a2f
root <root@LAPTOP-KE2O475O.>
2025-01-29 23:01:49 +0100

    file must be deleted

--- stack_b_move.c
+++ stack_b_move.c
@@ -6,63 +6,10 @@
 /*   By: jdorazio <jdorazio@student.42.madrid.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 17:59:18 by jdorazio          #+#    #+#             */
-/*   Updated: 2025/01/28 18:55:46 by jdorazio         ###   ########.fr       */
+/*   Updated: 2025/01/29 23:01:14 by jdorazio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 
-void	r_top_b(t_stack *stacks, size_t index)
-{
-	size_t	median_A;
-	size_t	median_B;
-	size_t	rot_count_B;
-	size_t	rot_count_A;
-
-	median_A = stacks->size_a / 2;
-	median_B = stacks->size_b / 2;
-
-	// Calculate rotations needed for stack_b
-	if (index < median_B)
-		rot_count_B = index; // Rotations to bring index to the top
-	else
-		rot_count_B = stacks->size_b - index; // Reverse rotations to bring index to the top
-
-	// Calculate rotations needed for target in stack_a
-	if (stacks->target_b[index] < median_A)
-		rot_count_A = stacks->target_b[index]; // Rotations for target
-	else
-		rot_count_A = stacks->size_a - stacks->target_b[index]; // Reverse rotations for target
-	//printf("Index %d | Median B %d | Rot_count A: %d | Rot_count B: %d\n", (int) index, (int) median_B, (int) rot_count_A, (int) rot_count_B);
-	// Rotate both stacks simultaneously
-	while (rot_count_B > 0 && rot_count_A > 0)
-	{
-		if (index <= median_B && stacks->target_b[index] <= median_A)
-			rot(stacks, 2); // Rotate both stacks
-		else
-			rev_rot(stacks, 2); // Reverse rotate both stacks
-		rot_count_B--;
-		rot_count_A--;
-	}
-
-	// Rotate stack_b independently
-	while (rot_count_B > 0)
-	{
-		if (index < median_B)
-			rot(stacks, 1);
-		else
-			rev_rot(stacks, 1);
-		rot_count_B--;
-	}
-
-	// Rotate stack_a independently
-	while (rot_count_A > 0)
-	{
-		if (stacks->target_b[index] < median_A)
-			rot(stacks, 0);
-		else
-			rev_rot(stacks, 0);
-		rot_count_A--;
-	}
-}
--- stack_move.c
+++ stack_move.c
@@ -6,56 +6,37 @@
 /*   By: jdorazio <jdorazio@student.42.madrid.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 17:59:18 by jdorazio          #+#    #+#             */
-/*   Updated: 2025/01/28 19:12:47 by jdorazio         ###   ########.fr       */
+/*   Updated: 2025/01/29 22:39:52 by jdorazio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	sync_rrr(t_stack *stacks, size_t index)
+void	sync_rr_rrr(t_stack *stacks, size_t index, size_t rr_or_rrr)
 {
-	while (index > 0 && stacks->target_a[index] > 0)
-	{
-		rev_rot(stacks, 2);
-		index--;
-		stacks->target_a[index]--; // Update target index logically
-	}
-	while (index > 0)
-	{
-		rev_rot(stacks, 0);
-		index = (index == 0) ? stacks->size_a - 1 : index - 1; // Circular update
-	}
-	while (stacks->target_a[index] > 0)
-	{
-		rev_rot(stacks, 1);
-		stacks->target_a[index]--; // Update target index logically
-	}
-}
+	int	value_A;
+	int	value_B;
 
+	value_A = stacks->stack_a[index];
+	value_B = stacks->stack_b[stacks->target_a[index]];
 
-void	sync_rr(t_stack *stacks, size_t index)
-{
-	while (index > 0 && stacks->target_a[index] > 0)
+	if (rr_or_rrr == 1)
 	{
-		//printf("sync_rr\n");
-		//printf("Index en Stack move [%zu]\n", index);
-		rot(stacks, 2);
-		index--;
-		stacks->target_a[index]--;
+		while (value_A != stacks->stack_a[0] && stacks->stack_b[0] != value_B)
+			rot(stacks, 2);
+		while (value_A != stacks->stack_a[0])
+			rot(stacks, 0 );
+		while (stacks->stack_b[0] != value_B)
+			rot(stacks, 1);
 	}
-	while (index > 0)
+	else if (rr_or_rrr == 2)
 	{
-		//printf( "sync_rr: rotating stack a\n");
-		//printf("Index en Stack move [%zu]\n", index);
-		rot(stacks, 0 ),
-		index--;
-	}
-	while (stacks->target_a[index] > 0)
-	{
-		//printf("sync_rr: rotating stack b\n");
-		//printf("Index en Stack move [%zu]\n", index);
-		rot(stacks, 1);
-		stacks->target_a[index]--;
+		while (value_A != stacks->stack_a[0] && stacks->stack_b[0] != value_B)
+			rev_rot(stacks, 2);
+		while (value_A != stacks->stack_a[0])
+			rev_rot(stacks, 0 );
+		while (stacks->stack_b[0] != value_B)
+			rev_rot(stacks, 1);
 	}
 }
 
@@ -68,22 +49,18 @@ void	r_top(t_stack *stacks, size_t index)
 
 	while (index > 0)
 	{
-		//printf("r_top: Rotating Stack A\n");
-		//printf("Index en Stack move [%zu]\n", index);
-		if (index < median_A)
+		if (index <= median_A)
 			rot(stacks, 0);
 		else
 			rev_rot(stacks, 0);
 		index--;
 	}
-	//printf("Target A %d\n", (int) stacks->target_a[index]);
-	while (stacks->target_a[0] != stacks->target_a[index])
+	while (stacks->target_a[index] > 0)
 	{
-		//printf("r_top: Rotating Stack B\n");
-		//printf("Index en Stack move [%zu]\n", index);
-		if (stacks->target_a[index] < median_B)
+		if (stacks->target_a[index] <= median_B)
 			rot(stacks, 1);
 		else
 			rev_rot(stacks, 1);
+		stacks->target_a[index]--;
 	}
 }
