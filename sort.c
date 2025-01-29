d5080abfeb502cf221a1c2ebe6f87e8751fc081a
root <root@LAPTOP-KE2O475O.>
2025-01-29 23:04:23 +0100

    comments were erased. All sorts are working

--- sort.c
+++ sort.c
@@ -6,7 +6,7 @@
 /*   By: jdorazio <jdorazio@student.42.madrid.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 16:06:33 by jdorazio          #+#    #+#             */
-/*   Updated: 2025/01/28 19:14:34 by jdorazio         ###   ########.fr       */
+/*   Updated: 2025/01/29 22:54:55 by jdorazio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ void	sort_three(t_stack *stacks)
 
 	if (!stacks || stacks->size_a > 3)
 		return ;
-	max_value = find_max(stacks);
+	max_value = find_max(stacks->stack_a, stacks->size_a);
 	if (max_value == stacks->stack_a[0])
 		rot(stacks, 0);
 	else if (max_value == stacks->stack_a[1])
@@ -52,7 +52,7 @@ int	is_sorted_circular(t_stack *stacks)
 
 	i = 0;
 	size_a = stacks->size_a;
-	min_index = find_index(stacks, find_min(stacks));
+	min_index = find_index(stacks->stack_a, size_a, find_min(stacks));
 	while (i < size_a - 1)
 	{
 		if (stacks->stack_a[(min_index + i) % size_a] > stacks->stack_a[(min_index + i + 1) % size_a])
@@ -70,24 +70,17 @@ void	sort_circular_stack(t_stack *stacks)
 	size_t	rot_cost;
 	size_t	rr_cost;
 
-	//printf("starting sort circular stack\n");
 	size_a = stacks->size_a;
 	min_value = find_min(stacks);
-	min_index = find_index(stacks, min_value);
+	min_index = find_index(stacks->stack_a, size_a, min_value);
 	rot_cost = min_index;
 	rr_cost = size_a - min_index;
 	if (rot_cost < rr_cost)
-		while (find_index(stacks, min_value) != 0)
-		{
-			//printf("Rotating stack a\n");
+		while (find_index(stacks->stack_a, size_a, min_value) != 0)
 			rot(stacks, 0);
-		}
 	else
-		while (find_index(stacks, min_value) != 0)
-		{
-			//printf("Rev. Rot. stack a\n");
+		while (find_index(stacks->stack_a, size_a, min_value) != 0)
 			rev_rot(stacks, 0);
-		}
 }
 
 void	sort_stacks(t_stack *stacks)
@@ -106,37 +99,12 @@ void	sort_stacks(t_stack *stacks)
 		if (is_sorted_circular(stacks))
 			sort_circular_stack(stacks);
 		push(stacks, 1);
-
-	// printf("\n STack A to B\n");
-	// for (size_t i = 0; i < stacks->size_a; i++)
-	// 	printf("stack_a[%zu] = %d\n", i, stacks->stack_a[i]);
-	// printf("\nFinal Stack B:\n");
-	// for (size_t i = 0; i < stacks->size_b; i++)
-	// 	printf("stack_b[%zu] = %d\n", i, stacks->stack_b[i]);
-	// printf("\n");
 	}
-	
 	sort_three(stacks);
-	// printf("\nFinal Stack A:\n");
-	// for (size_t i = 0; i < stacks->size_a; i++)
-	// 	printf("stack_a[%zu] = %d\n", i, stacks->stack_a[i]);
-	// printf("\nFinal Stack B:\n");
-	// for (size_t i = 0; i < stacks->size_b; i++)
-	// 	printf("stack_b[%zu] = %d\n", i, stacks->stack_b[i]);
-	// printf("\n");
-	// printf("\n Start Sort from B to A \n");
 	while (stacks->size_b)
 	{
 		move_cheapest_b(stacks);
 		push(stacks, 0);
-	// printf("\nFinal Stack A:\n");
-	// for (size_t i = 0; i < stacks->size_a; i++)
-	// 	printf("stack_a[%zu] = %d\n", i, stacks->stack_a[i]);
-	// printf("\nFinal Stack B:\n");
-	// for (size_t i = 0; i < stacks->size_b; i++)
-	// 	printf("stack_b[%zu] = %d\n", i, stacks->stack_b[i]);
-	// printf("\n");
 	}
-	// printf("\n Before las sort\n");
 	sort_circular_stack(stacks);
 }
